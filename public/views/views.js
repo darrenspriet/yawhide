84d4dc8e89c3704982bf09ad7954111c41382c8e
Darren Spriet <darrenspriet@gmail.com>
2013-12-05 23:39:26 -0500

    Added boot strap

--- public/views/views.js
+++ public/views/views.js
@@ -1,39 +1,24 @@
 var IndexView = Backbone.View.extend({
+	el:' #page_container',
+		events: {
+		"click #findFlyers": "findFlyersPage"
+	},
 	render: function(){
-		var sobeysFlyer = new GetSobeysFlyer();
-		sobeysFlyer.fetch({
-			success: function(){
-				console.log(sobeysFlyer);
-				// var sobeysArray= new Array();
-				// for(var i= 0; i<sobeysFlyer.attributes.items.length; i++){
-				// 	var sobeysObject =({
-				// 		"id": sobeysFlyer.attributes.items[i]._id,
-				// 		"description" : sobeysFlyer.attributes.items[i].description,
-				// 		"name" : sobeysFlyer.attributes.items[i].name,
-				// 		"price" : sobeysFlyer.attributes.items[i].price,
-				// 		"savings" : sobeysFlyer.attributes.items[i].savings
-				// 	})
-				// 	sobeysArray.push(sobeysObject);
-				// }
-				// console.log(sobeysArray);
+				$.get('templates/home.html', function(incomingTemplate){
+					var template = Handlebars.compile(incomingTemplate);
+					$('#page_container').html(template()).trigger('create');
+				});
+				return this;
+	}, 
+	findFlyersPage: function(){
+		                	app_router.navigate('#/nearestStores', {trigger: true});
 
-				// $.get('templates/index.html', function(incomingTemplate){
-				// 	var template = Handlebars.compile(incomingTemplate);
-				// 	$('#page_container').html(template({sobeysArray:sobeysArray})).trigger('create');
-
-				// });
-				// return this;
-			},
-			error: function(){
-				console.log('there was an error');
-
-			}
-		})
 	}
 });
 
 NearestStoresView = Backbone.View.extend({
 	el:' #page_container',
+
 	render: function(){
 		getLocation(function (loc){
 			var nearestSobeysStores = new GetNearestSobeys({elat: loc.latitude, elong: loc.longitude, maxD:50});
@@ -41,12 +26,32 @@ NearestStoresView = Backbone.View.extend({
 			nearestSobeysStores.fetch({
 				success: function(){
 					 console.log(nearestSobeysStores);
+					 var storesArray = new Array();
+					 for(var i=0;i<nearestSobeysStores.length;i++){
+					 		var storeObj = new Object({
+					 			"storeName" : nearestSobeysStores.models[i].attributes.storeName,
+					 			"storeNumber" : nearestSobeysStores.models[i].attributes.storeNumber
+					 		});
+					 		storesArray.push(storeObj);
+					 }
+					 console.log(storesArray);
 
 					$.get('../templates/nearestStores.html', function(incomingTemplate){
 						var template = Handlebars.compile(incomingTemplate);
 						$('#page_container').html(template).trigger('create');
 						google.maps.event.addDomListener(window, 'load', initializeMap(loc.latitude, loc.longitude));
+						var incomingStores =
+						    			"<table>"+
+    									"{{#storesArray}}"+
+    									"<tr><td>{{storeName}}</td><td>{{storeNumber}}</td></tr>"+
+    									"{{/storesArray}}"+
+    									"</table>";
+
+						var html = Mustache.to_html(incomingStores,{storesArray:storesArray} );
+						$('#stores').html(html).trigger('create');
 					})	
+
+					
 					return this;
 			},
 			error: function(){
